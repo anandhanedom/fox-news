1616bf8aeb0bc4c3d6162fddeeff0b367701c050
Anandhan <anandhanedom@gmail.com>
2020-10-14 10:24:17 +0530

    btn

--- src/pages/article/article.component.jsx
+++ src/pages/article/article.component.jsx
@@ -10,10 +10,18 @@ import './article.styles.css';
 import { selectArticle } from '../../redux/bookmarks/bookmarks.selectors.js';
 
 import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 
 const ArticlePage = ({ selectedArticle }) => {
   return (
-    <div style={{ marginTop: '100px' }}>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
       <div className="page-container">
         <div className="article">
           <div className="article__img">
@@ -23,24 +31,26 @@ const ArticlePage = ({ selectedArticle }) => {
             <h1>{selectedArticle.title}</h1>
             <p>{selectedArticle.author}</p>
             <p>{selectedArticle.publishedAt}</p>
-            <p>{selectedArticle.content}</p>
-            <Link
-              target="_blank"
-              style={{
-                background: '#000',
-                borderRadius: '25px',
-                padding: '10px 20px',
-                color: '#fff',
-                fontWeight: '700',
-                textTransform: 'unset',
-                cursor: 'pointer',
-                textDecoration: 'none',
-              }}
-              size="small"
-              href={`${selectedArticle.url}`}
-            >
-              Read More
-            </Link>
+            <p>{selectedArticle.content}</p> <br />
+            <Button>
+              <Link
+                target="_blank"
+                style={{
+                  background: '#000',
+                  borderRadius: '25px',
+                  padding: '10px 20px',
+                  color: '#fff',
+                  fontWeight: '700',
+                  textTransform: 'unset',
+                  cursor: 'pointer',
+                  textDecoration: 'none',
+                }}
+                size="small"
+                href={`${selectedArticle.url}`}
+              >
+                Read More
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
